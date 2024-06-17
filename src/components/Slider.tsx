ecca878556eed47d462439b33776340dd7ab307e
Week0312 <minamiwebcreate@gmail.com>
2024-06-17 23:49:17 +0900

    ♻️ Add a photo&Slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -4,27 +4,92 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./ProductSlider.css"; // スタイルシートをインポート
 
 const ProductSlider = () => {
     const settings = {
-        dots: true,
+        dots: false, // ドットナビゲーションを無効にする
         infinite: true,
-        speed: 500,
-        slidesToShow: 1,
+        speed: 8000, // スライドの速度をゆっくりに設定
+        slidesToShow: 3,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0, // スライドの切り替えを連続で行うために0に設定
+        cssEase: "linear", // 遷移効果をスムーズにする
     };
 
     return (
         <div className="slider-container">
             <Slider {...settings}>
                 <div>
-                    <img src="/images/product1.jpg" alt="Product 1" />
+                    <img
+                        src="/images/photo1.jpg"
+                        alt="Product 1"
+                        className="slider-image"
+                    />
                 </div>
                 <div>
-                    <img src="/images/product2.jpg" alt="Product 2" />
+                    <img
+                        src="/images/photo2.jpg"
+                        alt="Product 2"
+                        className="slider-image"
+                    />
                 </div>
                 <div>
-                    <img src="/images/product3.jpg" alt="Product 3" />
+                    <img
+                        src="/images/photo3.jpg"
+                        alt="Product 3"
+                        className="slider-image"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="/images/photo4.jpg"
+                        alt="Product 4"
+                        className="slider-image"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="/images/photo5.jpg"
+                        alt="Product 5"
+                        className="slider-image"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="/images/photo6.jpg"
+                        alt="Product 6"
+                        className="slider-image"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="/images/photo7.jpg"
+                        alt="Product 7"
+                        className="slider-image"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="/images/photo8.jpg"
+                        alt="Product 8"
+                        className="slider-image"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="/images/photo9.jpg"
+                        alt="Product 9"
+                        className="slider-image"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="/images/photo10.jpg"
+                        alt="Product 10"
+                        className="slider-image"
+                    />
                 </div>
             </Slider>
         </div>
